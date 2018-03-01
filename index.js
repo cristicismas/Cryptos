bc7baf3fa4a4486775d8f487f4a86e325ea43223
Cristi <cristicismas99@gmail.com>
2018-03-01 14:18:20 +0200

    Add media queries for responsive design

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ $(document).ready(function() {
 	})
 	.fail(function() {
 		console.log("Error");
-	})
-});
-
-function handleCryptoPricing(data) {
+	});
+	
+	function handleCryptoPricing(data) {
 	handleBTC(data);
 	handleETH(data);
 	handleXRP(data);
@@ -18,49 +17,50 @@ function handleCryptoPricing(data) {
 	handleDASH(data);
 	handleXEM(data);
 	handleDOGE(data);
-}
+	}
 
-function handleBTC(data) {
-	$("#btc .usd").text(data.BTC.USD);
-	$("#btc .eur").text(data.BTC.EUR);
-}
+	function handleBTC(data) {
+		$("#btc .usd").text(data.BTC.USD);
+		$("#btc .eur").text(data.BTC.EUR);
+	}
 
-function handleETH(data) {
-	$("#eth .usd").text(data.ETH.USD);
-	$("#eth .eur").text(data.ETH.EUR);
-}
+	function handleETH(data) {
+		$("#eth .usd").text(data.ETH.USD);
+		$("#eth .eur").text(data.ETH.EUR);
+	}
 
-function handleXRP(data) {
-	$("#xrp .usd").text(data.XRP.USD);
-	$("#xrp .eur").text(data.XRP.EUR);
-}
+	function handleXRP(data) {
+		$("#xrp .usd").text(data.XRP.USD);
+		$("#xrp .eur").text(data.XRP.EUR);
+	}
 
-function handleLTC(data) {
-	$("#ltc .usd").text(data.LTC.USD);
-	$("#ltc .eur").text(data.LTC.EUR);
-}
+	function handleLTC(data) {
+		$("#ltc .usd").text(data.LTC.USD);
+		$("#ltc .eur").text(data.LTC.EUR);
+	}
 
-function handleXMR(data) {
-	$("#xmr .usd").text(data.XMR.USD);
-	$("#xmr .eur").text(data.XMR.EUR);
-}
+	function handleXMR(data) {
+		$("#xmr .usd").text(data.XMR.USD);
+		$("#xmr .eur").text(data.XMR.EUR);
+	}
 
-function handleLSK(data) {
-	$("#lsk .usd").text(data.LSK.USD);
-	$("#lsk .eur").text(data.LSK.EUR);
-}
+	function handleLSK(data) {
+		$("#lsk .usd").text(data.LSK.USD);
+		$("#lsk .eur").text(data.LSK.EUR);
+	}
 
-function handleDASH(data) {
-	$("#dash .usd").text(data.DASH.USD);
-	$("#dash .eur").text(data.DASH.EUR);
-}
+	function handleDASH(data) {
+		$("#dash .usd").text(data.DASH.USD);
+		$("#dash .eur").text(data.DASH.EUR);
+	}
 
-function handleXEM(data) {
-	$("#xem .usd").text(data.XEM.USD);
-	$("#xem .eur").text(data.XEM.EUR);
-}
+	function handleXEM(data) {
+		$("#xem .usd").text(data.XEM.USD);
+		$("#xem .eur").text(data.XEM.EUR);
+	}
 
-function handleDOGE(data) {
-	$("#doge .usd").text(data.DOGE.USD);
-	$("#doge .eur").text(data.DOGE.EUR);
-}
+	function handleDOGE(data) {
+		$("#doge .usd").text(data.DOGE.USD);
+		$("#doge .eur").text(data.DOGE.EUR);
+	}
+});
